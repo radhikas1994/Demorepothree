c708eca037cce34a0141b42f23224e77287506e9
Radhikas1994 <radhikashinde3010@gmail.com>
2023-02-09 22:32:25 +0530

    my repo change

--- Temp/src/demo1/GithubProgram.java
+++ Temp/src/demo1/GithubProgram.java
@@ -5,6 +5,7 @@ public class GithubProgram {
 	public static void main(String[] args) {
        
 		System.out.println("Welcome to github program");
+		System.out.println("to change name my repo");
 
 	}
 
